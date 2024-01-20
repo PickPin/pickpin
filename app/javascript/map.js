077070c94d9c3d763f7443ec560d059988f26ce0
nowex35 <a.hayate0726@gmail.com>
2024-01-20 15:55:48 +0900

    地図画面追加

---
+++ app/javascript/map.js
@@ -0,0 +1,14 @@
+
+function initMap() {
+    const tsukuba = { lat: 36.109682, lng: 140.101583 }; // 石の広場を指定
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 18,
+      center: tsukuba,
+    });
+    const marker = new google.maps.Marker({
+      position: tsukuba,
+      map: map,
+    });
+  }
+  window.initMap = initMap;
+  
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# 追記
+pin "map"
