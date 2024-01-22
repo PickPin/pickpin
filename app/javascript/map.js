9d423784cbb181e4e8edb72aa0f3ae0c567afab9
hideki <itohisahideki@gmail.com>
2024-01-22 15:55:58 +0900

    Initial commit

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,3 +1,4 @@
+let lastMarker = null; //ユーザーがクリックした位置に作成されるmarker
 
 function initMap() {
     const tsukuba = { lat: 36.109682, lng: 140.101583 }; // 石の広場を指定
@@ -9,6 +10,27 @@ function initMap() {
       position: tsukuba,
       map: map,
     });
+    
+    map.addListener('click', function(e) {
+      getClickLatLng(e.latLng, map, lastMarker);
+    });
   }
-  window.initMap = initMap;
-  
+
+  function getClickLatLng(lat_lng, map) {
+    console.log(lat_lng.lat(), lat_lng.lng())
+
+    // 前のマーカーがあれば削除
+    if (lastMarker) {
+        lastMarker.setMap(null);
+    }
+
+    // 新しいマーカーを作成
+    lastMarker = new google.maps.Marker({
+      position: lat_lng,
+      map: map
+    });
+
+    map.panTo(lat_lng);
+}
+
+window.initMap = initMap;
