377ecd06391086fd184e55df8a19e4d4def5cf41
nowex35 <a.hayate0726@gmail.com>
2024-01-24 16:38:06 +0900

    浮いたバーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get "/map", to: "map#index"
-  # resources :map, only: [:index]
+  get "/map", to: "map#index"
+  resources :map, only: [:index]
   root "map#index"
   get 'debug', to: 'debug#show'
 end
