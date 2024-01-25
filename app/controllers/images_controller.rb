b021781cea95baddefa1a8479fb9cda477caf6e6
hideki <itohisahideki@gmail.com>
2024-01-25 11:54:02 +0900

    mac copy

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,29 @@
 class ImagesController < ApplicationController
-    def new
-        @image = Image.new
+  def index
+    @images = Image.all
+  end
+
+  def new
+      @image = Image.new
+  end
+
+  def create
+      @image = Image.new(image_params)
+      if @image.save
+        # 保存に成功した場合の処理
+        redirect_to root_path
+      else
+        # 保存に失敗した場合の処理
+        render :new
       end
-    
-      def create
-        @image = Image.new()
-    
-        if @image.save
-          # 保存に成功した場合の処理（例: リダイレクト）
-          redirect_to "/", notice: 'Image was successfully created.'
-        else
-          # 保存に失敗した場合の処理（例: 再度フォームを表示）
-          render :new
-        end
-    end
+  end
+
+  def edit
+      @image = Image.find(params[:id])
+  end
+
+  private
+  def image_params
+      params.require(:image).permit(:post_image, :comment, :user_id, :latitude, :longitude)
+  end
 end
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,5 @@
 class MapController < ApplicationController
   def index
+    @image = Image.new
   end
 end
