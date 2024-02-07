09b86266d6ae17453b3f4953e0e5de68a9c08e60
hideki <itohisahideki@gmail.com>
2024-02-07 22:45:18 +0900

    test

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,7 +1,2 @@
 class RegistrationController < ApplicationController
-    #deviceのコントローラーを継承
-    super
-    if account_update_params[:icon].present?
-      resource.icon.attach(account_update_params[:icon])    
-    end
 end
