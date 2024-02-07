01ee244a6b178c012a77a6713701610fa7433db2
hideki <itohisahideki@gmail.com>
2024-02-07 22:40:56 +0900

    test

--- config/puma.rb
+++ config/puma.rb
@@ -24,8 +24,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 # ここコメントアウト
-port ENV.fetch("PORT") { 3000 }
-# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# port ENV.fetch("PORT") { 3000 }
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
