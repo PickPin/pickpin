858b862b88e4799ac61a9405f8b045efad7f8fe9
EC2 Default User <ec2-user@ip-172-31-47-16.ap-northeast-1.compute.internal>
2024-02-07 13:40:02 +0000

    神頼み

--- config/puma.rb
+++ config/puma.rb
@@ -23,19 +23,9 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-<<<<<<< HEAD
-<<<<<<< HEAD
-# port ENV.fetch("PORT") { 3000 }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"q
-=======
-# port ENV.fetch("PORT") { 3000 }　ここコメントアウト
+
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
->>>>>>> origin/master
-=======
-# ここコメントアウト
-port ENV.fetch("PORT") { 3000 }
-# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
->>>>>>> origin/master
+
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
